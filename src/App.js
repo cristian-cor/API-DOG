cd99fe3ba36841dd3057840773275f9455761ffc
cristian-cor <19correa99@gmail.com>
2022-02-21 15:28:34 -0500

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import './index.css'
 
 function App() {
+
+  const [api, setApi] = useState()
+  const [next, setNext] = useState(false)
+
+useEffect ( () => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+    .then(response => response.json())
+    .then(data => setApi(data.message))
+}, [next] )
+
+const siguiente = () => {
+  setNext(!next)
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button type='button' onClick={siguiente}>Next</button>
+      <img src={api} className="img-fluid" alt="f"/>
     </div>
   );
 }
